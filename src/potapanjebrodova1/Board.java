a837acc2fe06c1c9289c3d9ed79078b06c1e486b
draganapeter <dragana.peter@gmail.com>
2014-05-15 00:57:57 +0200

    https://github.com/draganapeter/battleship.git

--- src/potapanjebrodova1/Board.java
+++ src/potapanjebrodova1/Board.java
@@ -1,52 +1,117 @@
+package potapanjebrodova1;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package potapanjebrodova1;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import javax.swing.JPanel;
-
 /**
- *
- * @author S
- */
+*
+* @author Administrator
+*/
 public class Board extends JPanel implements Runnable {
-    public final int PANEL_WIDTH = 600;  // širina table
-     public final int PANEL_HEIGHT = 600;  // Visina table
     
-    final Color BACKGROUND_COLOR = new Color(0,250,107);
-  
+     /**
+* Širina table
+*/
+    public final int PANEL_WIDTH = 800;
+    /**
+* Visina table
+*/
+    public final int PANEL_HEIGHT = 800;
+    
+    final Color BACKGROUND_COLOR = new Color(75,250,70);
+    final Thread runner;
+    
+      Boolean inGame;
       
+    // Objekti u igri
+      Polje ball;
+   
+    String message;
     
+    /**
+* Podrazumjevani konstruktor. Postavlja veličinu table, boju pozadine i font,
+* inicijalizuje početni rezultat, te objekte u igri. Inicijalizuje i pokreće
+* radnu nit.
+*/
     public Board() {
-       
         setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         setBackground(BACKGROUND_COLOR);
         setFocusable(true);
         setFont(getFont().deriveFont(Font.BOLD, 18f));
         setDoubleBuffered(true);
         
-       
+        inGame = false;
+        message = "BattleShip";
+        
+        ball = new Polje(this);
+                
+        runner = new Thread(this);
+        runner.start();
+    }
+    
+  
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+             
+         Graphics2D g2 = (Graphics2D) g;
+        
+        if (inGame) {
+            // Savjeti pri iscrtavanju
+        
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // Iscrtaj teren
+
+            g2.drawRect(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+          
+
+            // Iscrtaj sve objekte
+
+            ball.draw(g2);
+                           
+
+            // Sinhronizovanje sa grafičkom kartom
+            Toolkit.getDefaultToolkit().sync();
+
+            // Optimizacija upotrebe RAM-a,
+            g.dispose();
+        } else {
+            int messageWidth = getFontMetrics(getFont()).stringWidth(message);
+            g2.drawString(message, PANEL_WIDTH/2 - messageWidth/2, PANEL_HEIGHT/2);
+        }
+    }
+
+ private void update() {
+        //ball.move();
+        
     }
 
     @Override
-        public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void run() {
+               
+        while(true) {
+            update();
+            repaint();
+        }
     }
 
     void startGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       inGame = true;
     }
-
     
-         
-    /**
-     *
-     */
-  
+ 
 }
