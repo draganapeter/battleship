b4d16c296ac7d9abd4771a91df9b297946678216
draganapeter <dragana.peter@gmail.com>
2014-05-16 13:41:33 +0200

    https://github.com/draganapeter/battleship.git

---
+++ src/potapanjebrodova1/SquereRx.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package potapanjebrodova1;
+
+class SquareRx {
+    private final int row;
+    private final int col;
+    
+    
+            
+    public SquareRx(int r, int c) {
+       row = r;
+       col = c;
+    }
+ 
+}
