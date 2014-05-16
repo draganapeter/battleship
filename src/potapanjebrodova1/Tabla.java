426f21f27d2738dbcd9605e7a44554061cd2e5aa
draganapeter <dragana.peter@gmail.com>
2014-05-16 21:31:04 +0200

    https://github.com/draganapeter/battleship.git

--- src/potapanjebrodova1/Tabla.java
+++ src/potapanjebrodova1/Tabla.java
@@ -6,14 +6,47 @@
 
 package potapanjebrodova1;
 
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
 
 
 /**
  *
- * @author S
+ * @author s
  */
-public class Tabla {
+public class Tabla extends JPanel implements Objects {
     
+  Polje[][] polja;
   
-   
+  public Tabla(){
+      int ROWS = 10;
+      int COLS = 10;
+      polja = new Polje[ROWS][COLS];
+      for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+                polja[i][j] = new Polje(i, j);
+            }
+        }
+      
+  }
+  
+
+    @Override
+    public void move() {
+        
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+    int ROWS = 10;
+      int COLS = 10;
+    for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+                polja[i][j].draw(g2);
+            }
+    }
 }
+}
+   
+
